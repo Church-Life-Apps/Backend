f98eaf59cd68d154a354787c9160a39cb4036c11
MiTo0o <76019114+MiTo0o@users.noreply.github.com>
2023-01-23 15:13:38 -0600

    try diff port

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, '../web-build')));
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 app.get('/hi', (req, res) => {
   res.status(200).send('hello');
