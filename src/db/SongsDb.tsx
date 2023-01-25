ca15dd11d458d82503d6e066f9993bcbfb52e856
Brandon Xia <brandonxia01@gmail.com>
2023-01-25 17:47:52 -0500

    add log and fix

--- src/db/SongsDb.tsx
+++ src/db/SongsDb.tsx
@@ -1,9 +1,17 @@
 import { Pool } from 'pg'
 require('dotenv').config();
 
+console.log("env var host = " + process.env.PG_HOST);
+console.log("env var host = " + process.env.PG_PORT);
+console.log("env var user = " + process.env.PG_USER);
+console.log("env var pass = " + process.env.PG_PASSWORD);
+console.log("env var data = " + process.env.PG_DATABASE);
+console.log("env var environment = " + process.env.ENVIRONMENT);
+
+
 const pool = new Pool({
     host: process.env.PG_HOST,
-    port: (process.env.PORT || 25060) as number,
+    port: (process.env.PG_PORT || 25060) as number,
     user: process.env.PG_USER,
     password: process.env.PG_PASSWORD,
     database: process.env.PG_DATABASE,
