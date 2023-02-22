523b0789e4f17eb8d3344aa01c153d2b2111bff3
Brandon Xia <brandonxia01@gmail.com>
2023-02-22 15:15:59 -0500

    write util and db test cases

--- src/db/SongsDb.ts
+++ src/db/SongsDb.ts
@@ -2,7 +2,6 @@
  * Database Layer Code
  */
 
-import {it} from 'node:test';
 import {Pool} from 'pg';
 import {DatabaseError} from '../helpers/ErrorHelpers';
 import {formatForDbEntry} from '../utils/StringUtils';
@@ -18,7 +17,7 @@ import {
 require('dotenv').config();
 
 // Setup
-const pool = new Pool({
+const defaultPool = new Pool({
   host: process.env.PG_HOST,
   port: (process.env.PG_PORT || 25060) as number,
   user: process.env.PG_USER,
@@ -29,199 +28,206 @@ const pool = new Pool({
   },
 });
 
-/**
- * Queries all rows from songbooks table
- */
-export async function querySongbooks(): Promise<DbSongbook[]> {
-  return await queryDb(QUERY_SELECT_FROM_SONGBOOKS).then((rows) => {
-    return rows.map((row) => mapDbSongbook(row));
-  });
-}
+export class SongsDb {
+  private pool: Pool;
 
-/**
- * Lists all songs for a songbook
- */
-export async function querySongsForSongbook(
-  songbookId: string
-): Promise<DbSong[]> {
-  return await queryDb(buildGetSongsForSongbookQuery(songbookId)).then(
-    (rows) => {
-      return rows.map((row) => mapDbSong(row));
+  constructor(pool: Pool | null) {
+    if (pool == null) {
+      this.pool = defaultPool;
+    } else {
+      this.pool = pool;
     }
-  );
-}
+  }
 
-/**
- * Queries for a song with its lyrics for a song based on songbook and number
- */
-export async function querySongWithLyrics(
-  songbookId: string,
-  number: number
-): Promise<DbSongWithLyrics> {
-  return await queryDb(buildGetSongWithLyricsQuery(songbookId, number)).then(
-    (rows) => {
+  /**
+   * Queries all rows from songbooks table
+   */
+  async querySongbooks(): Promise<DbSongbook[]> {
+    return await this.queryDb(QUERY_SELECT_FROM_SONGBOOKS).then((rows) => {
+      return rows.map((row) => this.mapDbSongbook(row));
+    });
+  }
+
+  /**
+   * Lists all songs for a songbook
+   */
+  async querySongsForSongbook(songbookId: string): Promise<DbSong[]> {
+    return await this.queryDb(buildGetSongsForSongbookQuery(songbookId)).then(
+      (rows) => {
+        return rows.map((row) => this.mapDbSong(row));
+      }
+    );
+  }
+
+  /**
+   * Queries for a song with its lyrics for a song based on songbook and number
+   */
+  async querySongWithLyrics(
+    songbookId: string,
+    number: number
+  ): Promise<DbSongWithLyrics> {
+    return await this.queryDb(
+      buildGetSongWithLyricsQuery(songbookId, number)
+    ).then((rows) => {
       if (rows.length <= 0) {
         throw new DatabaseError(`Song not found for ${songbookId}: ${number}`);
       }
-      return mapDbSongWithLyric(rows);
-    }
-  );
-}
+      return this.mapDbSongWithLyric(rows);
+    });
+  }
 
-/**
- * Inserts a DbSongbook into the songbooks table
- */
-export async function insertSongbook(
-  songbook: DbSongbook
-): Promise<DbSongbook> {
-  return await queryDb(
-    buildInsertSongbookQuery(
-      songbook.id,
-      songbook.fullName,
-      songbook.staticMetadataLink,
-      songbook.imageUrl
-    )
-  ).then((rows) => {
-    if (rows.length > 0) {
-      return rows.map((row) => mapDbSongbook(row))[0];
-    } else {
-      throw new DatabaseError('Unable to insert Songbook');
-    }
-  });
-}
+  /**
+   * Inserts a DbSongbook into the songbooks table
+   */
+  async insertSongbook(songbook: DbSongbook): Promise<DbSongbook> {
+    return await this.queryDb(
+      buildInsertSongbookQuery(
+        songbook.id,
+        songbook.fullName,
+        songbook.staticMetadataLink,
+        songbook.imageUrl
+      )
+    ).then((rows) => {
+      if (rows.length > 0) {
+        return rows.map((row) => this.mapDbSongbook(row))[0];
+      } else {
+        throw new DatabaseError('Unable to insert Songbook');
+      }
+    });
+  }
 
-/**
- * Inserts a DbSong into the songs table
- */
-export async function insertSong(song: DbSong): Promise<DbSong> {
-  return await queryDb(
-    buildInsertSongQuery(
-      song.id,
-      song.songbookId,
-      song.number,
-      formatForDbEntry(song.title),
-      formatForDbEntry(song.author),
-      formatForDbEntry(song.music),
-      song.presentationOrder,
-      song.imageUrl
-    )
-  ).then((rows) => {
-    if (rows.length > 0) {
-      return mapDbSong(rows[0]);
-    } else {
-      throw new DatabaseError('Unable to insert song.');
-    }
-  });
-}
+  /**
+   * Inserts a DbSong into the songs table
+   */
+  async insertSong(song: DbSong): Promise<DbSong> {
+    return await this.queryDb(
+      buildInsertSongQuery(
+        song.id,
+        song.songbookId,
+        song.number,
+        formatForDbEntry(song.title),
+        formatForDbEntry(song.author),
+        formatForDbEntry(song.music),
+        song.presentationOrder,
+        song.imageUrl
+      )
+    ).then((rows) => {
+      if (rows.length > 0) {
+        return this.mapDbSong(rows[0]);
+      } else {
+        throw new DatabaseError('Unable to insert song.');
+      }
+    });
+  }
 
-/**
- * Inserts a DbLyric into the lyrics table
- */
-export async function insertLyric(lyric: DbLyric): Promise<DbLyric> {
-  return await queryDb(
-    buildInsertLyricQuery(
-      lyric.songId,
-      lyric.lyricType,
-      lyric.verseNumber,
-      formatForDbEntry(lyric.lyrics)
-    )
-  ).then((rows) => {
-    if (rows.length > 0) {
-      return mapDbLyric(rows[0]);
-    } else {
-      throw new DatabaseError('Unable to insert lyric');
-    }
-  });
-}
+  /**
+   * Inserts a DbLyric into the lyrics table
+   */
+  async insertLyric(lyric: DbLyric): Promise<DbLyric> {
+    return await this.queryDb(
+      buildInsertLyricQuery(
+        lyric.songId,
+        lyric.lyricType,
+        lyric.verseNumber,
+        formatForDbEntry(lyric.lyrics)
+      )
+    ).then((rows) => {
+      if (rows.length > 0) {
+        return this.mapDbLyric(rows[0]);
+      } else {
+        throw new DatabaseError('Unable to insert lyric');
+      }
+    });
+  }
 
-/**
- * Maps a database row to a DbSongbook object.
- */
-function mapDbSongbook(row: any): DbSongbook {
-  return {
-    id: row.id ?? '',
-    fullName: row.full_name ?? '',
-    staticMetadataLink: row.static_metadata_link ?? '',
-    imageUrl: row.image_url ?? '',
-  };
-}
+  /**
+   * Maps a database row to a DbSongbook object.
+   */
+  private mapDbSongbook(row: any): DbSongbook {
+    return {
+      id: row.id ?? '',
+      fullName: row.full_name ?? '',
+      staticMetadataLink: row.static_metadata_link ?? '',
+      imageUrl: row.image_url ?? '',
+    };
+  }
 
-/**
- * Maps a database row to a DbSong object
- */
-function mapDbSong(row: any): DbSong {
-  return {
-    id: row.id ?? '',
-    songbookId: row.songbook_id ?? '',
-    number: row.number ?? 0,
-    title: row.title ?? '',
-    author: row.author ?? '',
-    music: row.music ?? '',
-    presentationOrder: row.presentation_order ?? '',
-    imageUrl: row.image_url ?? '',
-  };
-}
+  /**
+   * Maps a database row to a DbSong object
+   */
+  private mapDbSong(row: any): DbSong {
+    return {
+      id: row.id ?? '',
+      songbookId: row.songbook_id ?? '',
+      number: row.number ?? 0,
+      title: row.title ?? '',
+      author: row.author ?? '',
+      music: row.music ?? '',
+      presentationOrder: row.presentation_order ?? '',
+      imageUrl: row.image_url ?? '',
+    };
+  }
 
-/**
- * Maps a database row to a DbLyric object
- */
-function mapDbLyric(row: any): DbLyric {
-  return {
-    songId: row.song_id ?? '',
-    lyricType: row.lyric_type ?? '',
-    verseNumber: row.verse_number ?? '',
-    lyrics: row.lyrics ?? '',
-  };
-}
+  /**
+   * Maps a database row to a DbLyric object
+   */
+  private mapDbLyric(row: any): DbLyric {
+    return {
+      songId: row.song_id ?? '',
+      lyricType: row.lyric_type ?? '',
+      verseNumber: row.verse_number ?? '',
+      lyrics: row.lyrics ?? '',
+    };
+  }
 
-/**
- * Maps a set of rows to a DbSongWithLyric object.
- * Requires that all the lyrics are associated with the same song.
- */
-function mapDbSongWithLyric(rows: any): DbSongWithLyrics {
-  return {
-    song: mapDbSong(rows[0]),
-    lyrics: rows
-      .filter((row: any) => {
-        return mapDbLyric(row).lyrics.length > 0;
+  /**
+   * Maps a set of rows to a DbSongWithLyric object.
+   * Requires that all the lyrics are associated with the same song.
+   */
+  private mapDbSongWithLyric(rows: any): DbSongWithLyrics {
+    return {
+      song: this.mapDbSong(rows[0]),
+      lyrics: rows
+        .filter((row: any) => {
+          return this.mapDbLyric(row).lyrics.length > 0;
+        })
+        .map((row: any) => this.mapDbLyric(row)),
+    };
+  }
+
+  /**
+   * Database query wrapper function
+   */
+  private async queryDb(query: string): Promise<any[]> {
+    console.log(`Database Query: "${query}".`);
+    return this.pool
+      .query(query)
+      .then((res) => {
+        console.log(`Returning ${res.rowCount} rows.`);
+        return res.rows;
       })
-      .map((row: any) => mapDbLyric(row)),
-  };
-}
+      .catch((err) => {
+        console.error(`Error due to: ${err}.`, err);
+        return [];
+      });
+  }
 
-/**
- * Database query wrapper function
- */
-async function queryDb(query: string): Promise<any[]> {
-  console.log(`Database Query: "${query}".`);
-  return pool
-    .query(query)
-    .then((res) => {
-      console.log(`Returning ${res.rowCount} rows.`);
-      return res.rows;
-    })
-    .catch((err) => {
-      console.error(`Error due to: ${err}.`, err);
-      return [];
-    });
-}
+  /**
+   * Database Query wrapper function which is expected to return no rows.
+   */
+  private async queryDbVoid(query: string): Promise<void> {
+    console.log(`Database Query: "${query}".`);
+    return this.pool
+      .query(query)
+      .then(() => {
+        console.log(`Query ran successfully.`);
+      })
+      .catch((err) => {
+        console.error(`Error due to: ${err}.`, err);
+      });
+  }
 
-/**
- * Database Query wrapper function which is expected to return no rows.
- */
-async function queryDbVoid(query: string): Promise<void> {
-  console.log(`Database Query: "${query}".`);
-  return pool
-    .query(query)
-    .then(() => {
-      console.log(`Query ran successfully.`);
-    })
-    .catch((err) => {
-      console.error(`Error due to: ${err}.`, err);
-    });
-}
-
-/** One time set up function. Never needs to be called again unless our DB gets nuked.
+  /** One time set up function. Never needs to be called again unless our DB gets nuked.
 export async function oneTimeSetUp(): Promise<void> {
     await queryDbVoid(QUERY_CREATE_SONGBOOKS_TABLE);
     await queryDbVoid(QUERY_CREATE_SONGS_TABLE);
@@ -229,3 +235,4 @@ export async function oneTimeSetUp(): Promise<void> {
     await queryDbVoid(QUERY_CREATE_LYRICS_TABLE);
 }
 */
+}
