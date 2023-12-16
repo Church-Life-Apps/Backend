03fca8467bb219f24273f4ad2f75be0c688bdf9c
Caleb Ziamba <cmz4by@virginia.edu>
2023-12-15 23:38:47 -0500

    remove old super-handler; trying to optimize song PUT with lyrics

--- src/lambdahandlers.ts
+++ src/lambdahandlers.ts
@@ -1,19 +1,13 @@
 import "dotenv/config";
 import { APIGatewayEvent, Handler } from "aws-lambda";
 
-import NotFoundError from "./errors/NotFoundError";
 import {
-  acceptPendingSong,
-  createPendingSong,
   createSong,
   createSongbook,
-  findSong,
   getSong,
   getSongbook,
-  listPendingSongs,
   listSongbooks,
   listSongs,
-  rejectPendingSong,
 } from "./api";
 import { Songbook } from "./models/ApiModels";
 
@@ -72,61 +66,4 @@ export const createSongHandler: Handler = async (event: APIGatewayEvent) => {
   const creationRequest = JSON.parse(event.body!);
   const number = parseSongNumber(event);
   return createSong(songbookId, number, creationRequest);
-};
-
-export const handler: Handler = async (event: APIGatewayEvent) => {
-  console.log("Received event:", JSON.stringify(event, null, 2));
-
-  let body;
-  const statusCode = "200";
-  const headers = {
-    "Content-Type": "application/json",
-  };
-
-  try {
-    switch (event.path) {
-      case "/pending/songs":
-        if (event.httpMethod === "GET") {
-          body = await listPendingSongs();
-        } else if (event.httpMethod === "POST") {
-          await createPendingSong(event);
-        } else {
-          throw new Error("Method not supported");
-        }
-        break;
-      case "/pending/songs/accept":
-        if (event.httpMethod === "GET") {
-          body = await acceptPendingSong(event);
-        } else {
-          throw new Error("Method not supported");
-        }
-        break;
-      case "/pending/songs/reject":
-        if (event.httpMethod === "GET") {
-          body = await rejectPendingSong(event);
-        } else {
-          throw new Error("Method not supported");
-        }
-        break;
-      case "/search":
-        if (event.httpMethod === "GET") {
-          body = await findSong(event);
-        } else {
-          throw new Error("Method not supported");
-        }
-        break;
-      default:
-        throw new NotFoundError();
-    }
-  } catch (err) {
-    // return formatErrorResponse(err);
-  } finally {
-    body = JSON.stringify(body);
-  }
-
-  return {
-    statusCode,
-    body,
-    headers,
-  };
 };
